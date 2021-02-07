e59e3f8a7ea27592b76f638382fb84a60f1140b3
alejandro <alejandro.juarez@tecso.coop>
2021-02-07 15:32:07 -0300

    Add change

--- src/server.js
+++ src/server.js
@@ -11,15 +11,15 @@ import mutationRouter from './routes/mutation.routes';
  */
 export default async function createServer(primaryPort) {
     const app = express();
-    
+
     // middlewares
     app.use(morgan('dev'));
     app.use(express.json());       // to support JSON-encoded bodies
     app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
-    
+
     // routes
     app.use('', mutationRouter);
-    
+
     const port = primaryPort || process.env.PORT || 3000;
     app.listen(port, () => {
         console.log(`Server run on port ${port}`)
