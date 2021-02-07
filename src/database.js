f832d2be7f7063402e541ea7797bf176575e1a2c
alejandro <alejandro.juarez@tecso.coop>
2021-02-07 15:22:36 -0300

    Add connection data to heroku

--- src/database.js
+++ src/database.js
@@ -6,10 +6,10 @@ import mongoose from 'mongoose';
  * For default has the local host and a default name that can be replace with environment paths
  */
 async function connect() {
-    const host = process.env.MONGO_HOST || 'localhost';
+    const host = process.env.MONGO_HOST || 'cluster0.fc4op.mongodb.net';
     const dbName = process.env.DB || 'prueba-teamknowlogy';
-    const dbProtocol = process.env.DB_PROTOCOL || 'mongodb';
-    const dbAuth = process.env.USER_PASSWORD || undefined;
+    const dbProtocol = process.env.DB_PROTOCOL || 'mongodb+srv';
+    const dbAuth = process.env.USER_PASSWORD || 'admin:admin';
 
     await mongoose.connect(`${dbProtocol}://${dbAuth ? dbAuth + '@' : ''}${host}/${dbName}?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
